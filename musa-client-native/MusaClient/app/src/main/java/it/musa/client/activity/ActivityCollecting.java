2476eae2e2314b741808d2c0c8bbb3cc7bcf171e
PCino <silvia.delpiano27@gmail.com>
2020-07-18 08:29:50 +0200

    BLE advertising added for ActivityTour and ActivityCollecting

--- musa-client-native/MusaClient/app/src/main/java/it/musa/client/activity/ActivityCollecting.java
+++ musa-client-native/MusaClient/app/src/main/java/it/musa/client/activity/ActivityCollecting.java
@@ -1,8 +1,22 @@
 package it.musa.client.activity;
 
+import android.annotation.TargetApi;
 import android.app.ProgressDialog;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseSettings;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
+
+import java.util.Arrays;
 
 import it.musa.client.R;
 import it.musa.client.vista.VistaCollecting;
@@ -13,14 +27,42 @@ public class ActivityCollecting extends AppCompatActivity {
 
     public static final String TAG = ActivityCollecting.class.getSimpleName();
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collecting);
+
+        // Create an AltBeacon BLE beacon
+        Beacon beacon = new Beacon.Builder()
+                .setId1("2f234454-cf6d-4a0f-adf2-f4911ba9ffa6")
+                .setId2("1")
+                .setId3("2")
+                .setManufacturer(0x0118)    // Radius network
+                .setTxPower(-59)
+                .setDataFields(Arrays.asList(new Long[] {0l}))
+                .build();
+
+        BeaconParser beaconParser = new BeaconParser()
+                .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
+
+        // Transmit beacons
+        BeaconTransmitter beaconTransmitter = new BeaconTransmitter(getApplicationContext(), beaconParser);
+        beaconTransmitter.startAdvertising(beacon, new AdvertiseCallback() {
+            @Override
+            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+                Log.i(TAG, "Advertisement start succeeded.");
+            }
+
+            @Override
+            public void onStartFailure(int errorCode) {
+                Log.e(TAG, "Advertisement start failed with code: "+ errorCode);
+            }
+        });
     }
 
     public VistaCollecting getVistaCollecting() {
         return (VistaCollecting) getSupportFragmentManager().findFragmentById(R.id.vistaCollecting);
     }
-
 }
+
--- musa-client-native/MusaClient/app/src/main/java/it/musa/client/activity/ActivityTour.java
+++ musa-client-native/MusaClient/app/src/main/java/it/musa/client/activity/ActivityTour.java
@@ -1,15 +1,57 @@
 package it.musa.client.activity;
 
+import android.annotation.TargetApi;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseSettings;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
+
+import java.util.Arrays;
 
 import it.musa.client.R;
 
 public class ActivityTour extends AppCompatActivity {
 
+    public static final String TAG = ActivityCollecting.class.getSimpleName();
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tour);
+
+        // Create an AltBeacon BLE beacon
+        Beacon beacon = new Beacon.Builder()
+                .setId1("2f234454-cf6d-4a0f-adf2-f4911ba9ffa6")
+                .setId2("1")
+                .setId3("2")
+                .setManufacturer(0x0118)    // Radius network
+                .setTxPower(-59)
+                .setDataFields(Arrays.asList(new Long[] {0l}))
+                .build();
+
+        BeaconParser beaconParser = new BeaconParser()
+                .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
+
+        // Transmit beacons
+        BeaconTransmitter beaconTransmitter = new BeaconTransmitter(getApplicationContext(), beaconParser);
+        beaconTransmitter.startAdvertising(beacon, new AdvertiseCallback() {
+            @Override
+            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+                Log.i(TAG, "Advertisement start succeeded.");
+            }
+
+            @Override
+            public void onStartFailure(int errorCode) {
+                Log.e(TAG, "Advertisement start failed with code: "+ errorCode);
+            }
+        });
+
     }
 }
