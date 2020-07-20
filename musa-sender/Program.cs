b8e534f31071f965eee42ed9b8e4ea64020bc554
unknown <stefano.foti@me.com>
2020-07-20 14:21:21 +0200

    bugfix

--- musa-sender/Program.cs
+++ musa-sender/Program.cs
@@ -15,9 +15,9 @@ namespace sender_cs
 
         private static TransportType transportType = TransportType.Mqtt;
 
-        private static string jsonMessage1 = "{\"timestamp\":\"2020-2xxx\",\"users\":[{\"mac\":\"A\",\"artworks\":[\"ID3\",\"ID3\",\"ID3\",\"ID3\",\"ID3\"]},{\"mac\":\"B\",\"artworks\":[\"2\",\"2\",\"2\",\"2\",\"2\"]}]}";
-        private static string jsonMessage2 = "{\"timestamp\":\"2020-2xxx\",\"users\":[{\"mac\":\"A\",\"artworks\":[\"ID3\",\"ID4\",\"ID4\",\"ID4\",\"ID4\"]},{\"mac\":\"B\",\"artworks\":[\"2\",\"2\",\"2\",\"2\",\"2\"]}]}";
-        private static string jsonMessage3 = "{\"timestamp\":\"2020-2xxx\",\"users\":[{\"mac\":\"A\",\"artworks\":[\"ID4\",\"ID4\",\"ID4\",\"ID5\",\"ID5\"]},{\"mac\":\"B\",\"artworks\":[\"2\",\"2\",\"2\",\"2\",\"2\"]}]}";
+        private static string jsonMessage1 = "{\"timestamp\":\"X\",\"users\":[{\"id\":\"A\",\"artworks\":[\"ID3\",\"ID3\",\"ID3\",\"ID3\",\"ID3\"]},{\"id\":\"B\",\"artworks\":[\"2\",\"2\",\"2\",\"2\",\"2\"]}]}";
+        private static string jsonMessage2 = "{\"timestamp\":\"X\",\"users\":[{\"id\":\"A\",\"artworks\":[\"ID3\",\"ID4\",\"ID4\",\"ID4\",\"ID4\"]},{\"id\":\"B\",\"artworks\":[\"2\",\"2\",\"2\",\"2\",\"2\"]}]}";
+        private static string jsonMessage3 = "{\"timestamp\":\"X\",\"users\":[{\"id\":\"A\",\"artworks\":[\"ID4\",\"ID4\",\"ID4\",\"ID5\",\"ID5\"]},{\"id\":\"B\",\"artworks\":[\"2\",\"2\",\"2\",\"2\",\"2\"]}]}";
         private static string []messages = {jsonMessage1, jsonMessage2, jsonMessage3};
 
         public static void Main(string[] args)
